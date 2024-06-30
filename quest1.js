b2343c45c31c06353dbfcceb3bca22cf7d279bcd
Ashleywairimu92 <ashleywairimu18@gamil.com>
2024-06-30 23:59:39 +0300

    wk1-code-challage

---
+++ quest1.js
@@ -0,0 +1,18 @@
+
+let score = 45;
+
+if (score > 79){
+    console.log('A');
+}else if (score>60 && score < 79){
+    console.log('B');
+}else if (score>49 && score< 59){
+    console.log('C');
+}else if (score >40 && score <49){
+    console.log('D');
+}else{
+    console.log('E');
+} 
+console.log(score);
+
+
+
---
+++ quest2.js
@@ -0,0 +1,18 @@
+
+function calculateDemeritPoints(){
+    let speed = 150;
+    const speedLimit = 70;
+    let demeritPoints = 0;
+
+    if(speed<70){
+        console.log('Ok');
+    }else{
+        demeritPoints = Math.floor((speed-speedLimit)/5)
+        console.log(`points:${demeritPoints}`)
+    
+    if (demeritPoints>12){
+    console.log('Licence suspended')
+    }
+  }
+}
+calculateDemeritPoints()
